5eb604e04b3011bc1bfc87c01e4c5f26c787c4db
GifReplyy <hvisionx@gmail.com>
2023-09-29 10:11:10 +0200

    new

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -8,15 +8,10 @@ function setup() {
 
 function draw() {
   noStroke();
-  fill(0,0,255,0.3);
+  // fill(0,0,255,0.3);
   fill('red');
   rect(0,150,450,150);
-
-  // fill('green');
-  // fill(0,255,0);
-  
-  // fill(0, 128, 0);
-  fill(0, 0, 255)
+  fill('green');
   rect(0,0,150,450);
   fill('blue');
   rect(75,0,375,450);
