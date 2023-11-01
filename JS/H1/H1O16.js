9f6193416fc1fe61f8d711c5781b9668389dc4ed
GifReplyy <hvisionx@gmail.com>
2023-11-01 19:52:10 +0100

    1 11 23

--- JS/H1/H1O16.js
+++ JS/H1/H1O16.js
@@ -1,41 +1,24 @@
 var diameter = 300;
-
-var Yellipse1 = -50;
-var Yellipse2 = 500;
-
-var stop1 = false; // Variable om beweging ellips 1 te stoppen
-var stop2 = false; // Variable om beweging ellips 2 te stoppen
+var h = 0;
 
 function setup() {
-  canvas = createCanvas(450, 450);
+  canvas = createCanvas(450,450);
   canvas.parent('processing');
   colorMode(RGB, 255, 255, 255, 1);
   noStroke();
 }
 
 function draw() {
-  background(255, 255, 255, 1);
+  background(255,255,255,1);
 
   fill(255, 0, 0, 0.5);
   ellipse(width / 2, height / 2, diameter);
 
-  if (!stop1) {
-    Yellipse1 += 3;
-    if (Yellipse1 >= 225) {
-      stop1 = true;
-    }
-  }
+  fill(0, 255, 0, 0.5);
+  ellipse(width / 2, h, diameter);
 
-  fill(50, 205, 50, 0.5);
-  ellipse(width / 2, Yellipse1, diameter);
+  fill(0, 0, 255, 0.5);
+  ellipse(width / 2, height - h, diameter);
 
-  if (!stop2) {
-    Yellipse2 -= 3;
-    if (Yellipse2 <= 225) {
-      stop2 = true;
-    }
-  }
-
-  fill(175, 0, 130, 0.5);
-  ellipse(width / 2, Yellipse2, diameter);
+  h++;
 }
